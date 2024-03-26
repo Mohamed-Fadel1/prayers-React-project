bfac9c227ff61bc9475a40634560b7e5e6ce93eb
Mohamed Fadel <mf90705520@gmail.com>
2024-03-26 17:03:05 +0200

    upload

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+
+
+import { Container, CssBaseline } from '@mui/material'
+import './App.css'
+import MainContent from './Components/MainContent/MainContent'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useState } from 'react';
+
+
+function App() {
+
+ 
+
+  const [myMood , setMyMood] = useState(localStorage.getItem("currentMode") === null ? "light" : "dark");
+  const darkTheme = createTheme({
+    palette: {
+      mode: myMood,
+    },
+  });
+
+  return (
+    <>
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <div style={{ display : "flex" , justifyContent : "center" , width : "97vw"}}>
+  <Container maxWidth = "xl">
+
+  <MainContent setMyMood = {setMyMood}   />
+  </Container>
+
+</div>
+
+
+    </ThemeProvider>
+
+    </>
+  )
+}
+
+export default App
